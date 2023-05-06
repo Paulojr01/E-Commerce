040644dda61dc575297a2ec4fde55c1e044d88eb
Paulo Alves <106793350+Paulojr01@users.noreply.github.com>
2023-05-06 18:00:55 -0300

    Criado 2 user no banco usando userRepository.saveAll(Arrays.asList());

--- src/main/java/com/workshop/course/config/TestConfig.java
+++ src/main/java/com/workshop/course/config/TestConfig.java
@@ -1,14 +1,24 @@
 package com.workshop.course.config;
 
+import com.workshop.course.entities.User;
 import com.workshop.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.util.Arrays;
+
 @Configuration
 @Profile("test")
-public class TestConfig {
+public class TestConfig implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Override
+    public void run(String... args) throws Exception {
+        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+        userRepository.saveAll(Arrays.asList(u1, u2));
+    }
 }
