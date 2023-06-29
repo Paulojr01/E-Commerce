216fceafe53fade4fbc05cdc9d9f28b63957b2d9
Paulo Alves <106793350+Paulojr01@users.noreply.github.com>
2023-06-29 04:01:04 -0300

    criado endpoint user insert

--- src/main/java/com/workshop/course/resources/UserResources.java
+++ src/main/java/com/workshop/course/resources/UserResources.java
@@ -5,7 +5,9 @@ import com.workshop.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -26,6 +28,13 @@ public class UserResources {
     public ResponseEntity<User> findById(@PathVariable Long id){
         User obj = (User) service.findById(id);
         return  ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return  ResponseEntity.created(uri).body(obj);
     }
 
 }
