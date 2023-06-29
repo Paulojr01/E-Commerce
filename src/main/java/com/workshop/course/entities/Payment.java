7520c198cbaedc2e3944c35f29997b258a26d7cb
Paulo Alves <106793350+Paulojr01@users.noreply.github.com>
2023-06-29 03:43:16 -0300

    criado metodos subtotal e total

--- src/main/java/com/workshop/course/entities/Payment.java
+++ src/main/java/com/workshop/course/entities/Payment.java
@@ -1,5 +1,6 @@
 package com.workshop.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -16,6 +17,7 @@ public class Payment implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Instant moment;
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
