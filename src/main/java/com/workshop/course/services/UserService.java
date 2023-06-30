1c12bf47f90270149298d467db8957521f83d11a
Paulo Alves <106793350+Paulojr01@users.noreply.github.com>
2023-06-30 02:06:36 -0300

    Criado exception para update

--- src/main/java/com/workshop/course/services/UserService.java
+++ src/main/java/com/workshop/course/services/UserService.java
@@ -5,6 +5,7 @@ import com.workshop.course.entities.User;
 import com.workshop.course.repositories.UserRepository;
 import com.workshop.course.services.exceptions.DatabaseException;
 import com.workshop.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -45,9 +46,13 @@ public class UserService {
     }
 
     public User update (Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException((id));
+        }
     }
 
     private void updateData(User entity, User obj) {
